02e15e17bdc9614e24345a881d1a68d753958351
uzor1997 <uzor1997@gmail.com>
2020-07-05 16:54:32 +0100

    Making

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// import { without } from "lodash";
+// console.log(without);
+
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
