5dd7d22816a01c64d805b43e3a55446e62fceb86
uzor1997 <uzor1997@gmail.com>
2020-09-27 21:45:30 +0100

    deleting unnecessary files

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-// import { without } from "lodash";
-// console.log(without);
+const currentBackground = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
-let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let body = document.getElementById("gradient");
 
 function setGradient() {
-    body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value +")";
-    css.textContent = body.style.background + ";";
+   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    currentBackground.textContent = `${body.style.background};`
 }
           
-color1.addEventListener("input", setGradient);
+color1.addEventListener("input", () => setGradient());
 
-color2.addEventListener("input", setGradient);
+color2.addEventListener("input", () => setGradient());
 
-
-// My Attempt
-// function showStyle() {
-//     if (css.value = "") {
-//         let h3 = document.createElement("h3");
-//         let h4= document.createTextNode("( to right, " + color1.value + ", " + color2.value + " )");
-//         h3.appendChild(h4);
-//         css.appendChild(h3);
-//         css.value ="";
-//     }
-//
-
-
-
-const febunacci = () => {
-    let num = 0; 
-    let storage = []; 
-    for (let i=0; (num.toString().length) < 5 ; i++) {
-        storage.push(num);
-        if (num < 2) {
-            num ++;
-        } else {
-            num += storage[i-1];
-        }
-    }
-    console.log(storage);
-    return storage;
-}
-
-febunacci();
